e4efcbdc2b404ddc50f3d101fbc06e8445d95df8
Arkadiusz Klimek <arklimek@student.agh.edu.pl>
2025-06-13 21:59:51 +0200

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 export default defineConfig({
     base: '/Korzystanie-JS-SDK/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'src/login.html'),
+            }
+        }
+    },
     plugins: [    
         tailwindcss(),  
     ],
