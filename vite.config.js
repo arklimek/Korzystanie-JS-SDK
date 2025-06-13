ed7a8721de739ea6894419c46b164979baa00db7
Arkadiusz Klimek <arklimek@student.agh.edu.pl>
2025-06-13 22:59:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,16 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-
 import fg from 'fast-glob'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
+const repoName = 'Korzystanie-JS-SDK';
+
 const inputs = fg.sync('src/**/*.html', { cwd: __dirname, absolute: true })
 
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [    
         tailwindcss(),  
     ],
